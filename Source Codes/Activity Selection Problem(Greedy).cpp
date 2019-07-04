8e60300d1e18c68d3f4ffdf977d8e4be504e09c9
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2019-07-05 01:55:48 +0530

    iterative approach

--- Source Codes/Activity Selection Problem(Greedy).cpp
+++ Source Codes/Activity Selection Problem(Greedy).cpp
@@ -8,6 +8,17 @@ bool second(const pair<int, int>& a, const pair<int, int>& b)
 	return (a.second < b.second);
 }
 
+void greedy_activity_solution(vector<pair<int, int>> a) {
+	int n = a.size(), k=0;
+	cout << a[0].first << ":" << a[0].second << endl;
+	for (int i = 1;i < n;i++) {
+		if (a[i].first >= a[k].second) {
+			cout << a[i].first << ":" << a[i].second << endl;
+			k = i;
+		}
+	}
+}
+
 void recursive_activity_solution(vector<pair<int, int>> a, int k) {
 	int m = k + 1;
 	while (m < a.size() && a[m].first < a[k].second) {
@@ -34,7 +45,9 @@ int main() {
 	a.push_back(make_pair(8, 12));
 	a.push_back(make_pair(2, 14));
 	a.push_back(make_pair(12, 16));
+	//the input must be sorted on the finish time in ascending order
 	sort(a.begin(), a.end(), second);
 	recursive_activity_solution(a, 0);
+	greedy_activity_solution(a);
 	return 0;
 }
